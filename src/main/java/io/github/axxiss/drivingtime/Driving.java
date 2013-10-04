926f459075147d4c15b47e01c44c06d1a1da1c7f
Alexis Mas <vodkadechernobil@gmail.com>
2013-10-04 10:01:54 +0200

    Initial commit

---
+++ src/main/java/io/github/axxiss/drivingtime/Driving.java
@@ -0,0 +1,32 @@
+package io.github.axxiss.drivingtime;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alexis
+ * Date: 03/10/13
+ * Time: 13:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class Driving {
+
+    /**
+     * Maximum daily driving time, 9 hours.
+     */
+    public static final int DAILY = 9 * 60;
+
+
+    /**
+     * Maximum nonstop driving time, 4,5 hours
+     */
+    public static final int NONSTOP = 4 * 60 + 30;
+
+    /**
+     * * Maximum weekly driving time, 56 hours
+     */
+    public static final int WEEKLY = 56 * 60;
+
+    /**
+     * * Maximum fortnightly driving time, 90 hours
+     */
+    public static final int FORTNIGHTLY = 90 * 60;
+}
---
+++ src/main/java/io/github/axxiss/drivingtime/Rest.java
@@ -0,0 +1,13 @@
+package io.github.axxiss.drivingtime;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alexis
+ * Date: 03/10/13
+ * Time: 13:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class Rest {
+
+
+}
